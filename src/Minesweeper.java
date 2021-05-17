419a4a282db8ed1b7744d3cc6e02aa40dc8e9b88
Arios <ariosssmfm@gmail.com>
2021-05-18 02:43:40 +0300

    renaming

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -23,13 +23,12 @@ import javax.swing.WindowConstants;
 
 public class Minesweeper extends JFrame{
     
-    
-    private BoardPanel board_panel;
-    private TopLabel flagslabel,timelabel;
-    private RestartButton restartbutton;
-    private int number_unused_flags,remaining_buttons;
-    private  int board_size=9,number_of_bombs=10;
-    private Buttons buttons[][]=new Buttons[board_size][board_size];
+    private BoardPanel boardPanel;
+    private TopLabel flagsLabel, timeLabel;
+    private RestartButton restartButton;
+    private int numberOfUnusedFlags, remainingButtons;
+    private final int boardSize =16, numberOfBombs =40;
+    private final Buttons[][] buttons=new Buttons[boardSize][boardSize];
     private Timer timer;
     private final ImageIcon bombIcon=new ImageIcon(".\\src\\images\\bomb100px.png"),flagIcon=new ImageIcon(".\\src\\images\\flag100px.png");
     
@@ -43,90 +42,98 @@ public class Minesweeper extends JFrame{
         setSize(900,1000);
         setLocationRelativeTo(null);
         setComponents();
-        start_new_game();
+        startNewGame();
     }
     
     private void setComponents() {
-        add(board_panel=new BoardPanel(),BorderLayout.PAGE_END);
-        add(flagslabel=new TopLabel(),BorderLayout.LINE_START); 
-        add(timelabel=new TopLabel(),BorderLayout.LINE_END); 
-        add(restartbutton=new RestartButton(),BorderLayout.CENTER);        
+        add(boardPanel =new BoardPanel(),BorderLayout.PAGE_END);
+        add(flagsLabel =new TopLabel(),BorderLayout.LINE_START);
+        add(timeLabel =new TopLabel(),BorderLayout.LINE_END);
+        add(restartButton =new RestartButton(),BorderLayout.CENTER);
     }
 
-    private void start_new_game() {
-       number_unused_flags=number_of_bombs;
-       remaining_buttons=board_size*board_size;
-       flagslabel.setText(Integer.toString(number_unused_flags));
-       set_the_board();
-       create_bombs();
-       count_bombs_around_buttons();
-       timer=new Timer();
-       timer.schedule(new MyTimerTask(),0,1000);
-       restartbutton.setIcon(restartbutton.smileface);
+    private void startNewGame() {
+       numberOfUnusedFlags = numberOfBombs;
+       remainingButtons = boardSize * boardSize;
+       setTheBoard();
+       createBombs();
+       countBombsAroundButtons();
+       createTimer();
+       setComponentsContent();
+    }
+
+    private void setComponentsContent() {
+        restartButton.setIcon(restartButton.smileFace);
+        flagsLabel.setText(Integer.toString(numberOfUnusedFlags));
+    }
+
+    private void createTimer() {
+        timer=new Timer();
+        timer.schedule(new MyTimerTask(),0,1000);
+    }
+
+    private void setTheBoard() {
+        for(int i = 0; i< boardSize; i++)
+            for(int j = 0; j< boardSize; j++)
+                boardPanel.add(buttons[i][j]=new Buttons(i,j));
     }
     
-    private void set_the_board() {
-        for(int i=0; i<board_size; i++)
-            for(int j=0; j<board_size; j++)
-                board_panel.add(buttons[i][j]=new Buttons(i,j));
-    }
-    
-    private void create_bombs() {
+    private void createBombs() {
         Random rand = new Random(); 
         int bombs=0;
-        while(bombs<number_of_bombs){
-            int i=rand.nextInt(board_size);
-            int j=rand.nextInt(board_size);
-            if(!buttons[i][j].hasbomb){
-            buttons[i][j].hasbomb=true;
-            bombs++;
+        while(bombs< numberOfBombs){
+            int i=rand.nextInt(boardSize);
+            int j=rand.nextInt(boardSize);
+            if(!buttons[i][j].hasBomb){
+                buttons[i][j].hasBomb = true;
+                bombs++;
             }    
         }
     }
 
-    private void count_bombs_around_buttons() {
-        for(int i=0; i<board_size; i++)
-            for(int j=0; j<board_size; j++)
+    private void countBombsAroundButtons() {
+        for(int i = 0; i< boardSize; i++)
+            for(int j = 0; j< boardSize; j++)
                 for(int k=i-1; k<=i+1; k++)
                     for(int s=j-1; s<=j+1; s++)
-                         if((k>=0&&s>=0&&k<board_size&&s<board_size)&&!(k==i&&s==j))
-                             if(buttons[k][s].hasbomb)
-                                 buttons[i][j].bombsaroundme++;        
+                         if((k>=0&&s>=0&&k< boardSize &&s< boardSize)&&!(k==i&&s==j))
+                             if(buttons[k][s].hasBomb)
+                                 buttons[i][j].bombsAroundIt++;
     }
     
-    private void set_bombs_everywhere(){
-        for(int i=0; i<board_size; i++)
-            for(int j=0; j<board_size; j++)
-                if(buttons[i][j].hasbomb&&!buttons[i][j].hasflag)
+    private void setBombsEverywhere(){
+        for(int i = 0; i< boardSize; i++)
+            for(int j = 0; j< boardSize; j++)
+                if(buttons[i][j].hasBomb &&!buttons[i][j].hasFlag)
                     buttons[i][j].setIcon(bombIcon);
-    setgameover(restartbutton.loseface);     
+    gameOver(restartButton.loseFace);
     }
     
     
-    private void setgameover(ImageIcon icon) {
-        for(int i=0; i<board_size; i++)
-            for(int j=0; j<board_size; j++)
+    private void gameOver(ImageIcon icon) {
+        for(int i = 0; i< boardSize; i++)
+            for(int j = 0; j< boardSize; j++)
                 buttons[i][j].removeMouseListener(buttons[i][j]);
         timer.cancel();
-        restartbutton.setIcon(icon);
+        restartButton.setIcon(icon);
     }
 
     
     private void recursion(Buttons button) {
-        if(button.hasflag)return;
-        button.cancel_it();
-        if(button.bombsaroundme!=0) button.setText(String.valueOf(button.bombsaroundme)); 
-        else recursion_for_buttons_around(button);
-        if(--remaining_buttons==number_of_bombs)setgameover(restartbutton.winface);//Win!
+        if(button.hasFlag)return;
+        button.cancelButton();
+        if(button.bombsAroundIt !=0) button.setText(String.valueOf(button.bombsAroundIt));
+        else recursionForButtonsAround(button);
+        if(--remainingButtons == numberOfBombs) gameOver(restartButton.winFace);//Win!
     }
 
-    private void recursion_for_buttons_around(Buttons button) {
+    private void recursionForButtonsAround(Buttons button) {
         int i=button.i,j=button.j;
         for(int k=i-1; k<=i+1; k++){
-            if(k<0||k==board_size)continue;
+            if(k<0||k == boardSize)continue;
             for(int s=j-1; s<=j+1; s++){
-                if(s<0||s==board_size)continue;
-                if(!buttons[k][s].done_recursion)
+                if(s<0||s == boardSize)continue;
+                if(!buttons[k][s].hasDoneRecursion)
                 recursion(buttons[k][s]);
             }
         }
@@ -136,11 +143,11 @@ public class Minesweeper extends JFrame{
             private class Buttons extends JButton implements MouseListener{
                 
                 private final int i,j;
-                private int bombsaroundme=0;
-                private boolean hasbomb=false,hasflag=false,done_recursion=false;
+                private int bombsAroundIt = 0;
+                private boolean hasBomb = false, hasFlag = false, hasDoneRecursion = false;
 
                 private Buttons(int i, int j) {
-                    this.i=i;this.j=j;
+                    this.i=i; this.j=j;
                     addMouseListener(this);
                     setBorder(BorderFactory.createLineBorder(Color.black));
                     setBackground(Color.GRAY);
@@ -148,45 +155,48 @@ public class Minesweeper extends JFrame{
 
                 @Override public void mouseClicked(MouseEvent me) {
                             if(SwingUtilities.isRightMouseButton(me)){
-                                if(!hasflag){   
-                                setIcon(flagIcon);
-                                hasflag=true;
-                                flagslabel.setText(String.valueOf(--number_unused_flags));
+                                if(!hasFlag){
+                                    setIcon(flagIcon);
+                                    hasFlag = true;
+                                    flagsLabel.setText(String.valueOf(--numberOfUnusedFlags));
                                 }else{
-                                setIcon(null);
-                                hasflag=false; 
-                                flagslabel.setText(String.valueOf(++number_unused_flags));
+                                    setIcon(null);
+                                    hasFlag = false;
+                                    flagsLabel.setText(String.valueOf(++numberOfUnusedFlags));
                                 }
                             }
                             if(SwingUtilities.isLeftMouseButton(me)){
-                                if(!hasflag)
-                                if(hasbomb)set_bombs_everywhere();   
+                                if(!hasFlag)
+                                if(hasBomb) setBombsEverywhere();
                                 else recursion(this); 
                             }         
                 }
-                @Override public void mousePressed(MouseEvent me) {if(SwingUtilities.isLeftMouseButton(me))restartbutton.setIcon(restartbutton.pressedface);}
-                @Override public void mouseReleased(MouseEvent me) {if(SwingUtilities.isLeftMouseButton(me))restartbutton.setIcon(restartbutton.smileface);}
+                @Override public void mousePressed(MouseEvent me) {if(SwingUtilities.isLeftMouseButton(me))
+                    restartButton.setIcon(restartButton.pressedFace);}
+                @Override public void mouseReleased(MouseEvent me) {if(SwingUtilities.isLeftMouseButton(me))
+                    restartButton.setIcon(restartButton.smileFace);}
                 @Override public void mouseEntered(MouseEvent me) {setBackground(Color.LIGHT_GRAY);}
                 @Override public void mouseExited(MouseEvent me) {setBackground(Color.GRAY);}
 
-                private Color getMycolor() {
-                switch (bombsaroundme) {
-                    case 1: return Color.BLUE;
-                    case 2: return Color.GREEN;
-                    case 3: return Color.RED;
-                    case 4: return Color.ORANGE;
-                    default: return Color.BLACK;
-                }}
+                private Color getColor() {
+                    return switch (bombsAroundIt) {
+                        case 1 -> Color.BLUE;
+                        case 2 -> Color.GREEN;
+                        case 3 -> Color.RED;
+                        case 4 -> Color.ORANGE;
+                        default -> Color.BLACK;
+                    };
+                }
                            
                 @Override
                 public void setText(String string){
                     super.setText(string);
-                    setForeground(getMycolor());
-                    setFont(new Font("Arial",Font.BOLD,450/board_size));
+                    setForeground(getColor());
+                    setFont(new Font("Arial",Font.BOLD,450/ boardSize));
                 }  
 
-                private void cancel_it() {
-                    done_recursion=true;
+                private void cancelButton() {
+                    hasDoneRecursion =true;
                     setBackground(Color.LIGHT_GRAY);
                     removeMouseListener(this);
                 }
@@ -206,15 +216,15 @@ public class Minesweeper extends JFrame{
             private class BoardPanel extends JPanel{
                 private BoardPanel() {
                     setPreferredSize(new Dimension(900,900));
-                    setLayout(new GridLayout(board_size,board_size));
+                    setLayout(new GridLayout(boardSize, boardSize));
                 }   
             }
             
             private class RestartButton extends JButton implements ActionListener{
-                private final ImageIcon smileface=new ImageIcon(".\\src\\images\\smileface.png");
-                private final ImageIcon pressedface=new ImageIcon(".\\src\\images\\pressedface.png");
-                private final ImageIcon loseface=new ImageIcon(".\\src\\images\\loseface.png");
-                private final ImageIcon winface=new ImageIcon(".\\src\\images\\winface.png");
+                private final ImageIcon smileFace =new ImageIcon(".\\src\\images\\smileface.png");
+                private final ImageIcon pressedFace =new ImageIcon(".\\src\\images\\pressedface.png");
+                private final ImageIcon loseFace =new ImageIcon(".\\src\\images\\loseface.png");
+                private final ImageIcon winFace =new ImageIcon(".\\src\\images\\winface.png");
                 
                 private RestartButton(){    
                     setPreferredSize(new Dimension(100,100));
@@ -224,18 +234,18 @@ public class Minesweeper extends JFrame{
                 @Override
                 public void actionPerformed(ActionEvent ae) {
                     timer.cancel();
-                    board_panel.removeAll();
-                    start_new_game();
-                    board_panel.revalidate();
-                    board_panel.repaint();
+                    boardPanel.removeAll();
+                    startNewGame();
+                    boardPanel.revalidate();
+                    boardPanel.repaint();
                 }
             }
             
             private class MyTimerTask extends TimerTask{
-                private int seconds_count=0; 
+                private int secondsPassed =0;
                 @Override
                 public void run() {
-                    timelabel.setText(String.valueOf(seconds_count++));
+                    timeLabel.setText(String.valueOf(secondsPassed++));
                 }
             } 
 
