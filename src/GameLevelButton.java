053e5eb5964ca33e9a635533c78ad65e6c3a2b7e
Arios <ariosssmfm@gmail.com>
2021-05-18 21:43:39 +0300

    GameLevelButton

---
+++ src/GameLevelButton.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GameLevelButton extends JButton implements ActionListener {
+
+    GameLevel gameLevel;
+
+    public GameLevelButton(GameLevel gameLevel) {
+        this.gameLevel = gameLevel;
+        setBackground(Color.BLACK);
+        setText(gameLevel.getLevelText());
+        addActionListener(this);
+    }
+
+    @Override
+    public void setText(String string){
+        super.setText(string);
+        setForeground(Color.WHITE);
+        setFont(new Font("Arial",Font.BOLD,40));
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        new MinesweeperGame(gameLevel).setVisible(true);
+    }
+}
--- src/MainMenu.java
+++ src/MainMenu.java
@@ -4,43 +4,34 @@ import java.awt.*;
 
 public class MainMenu extends JFrame {
 
+    private GameLevel easyLevel;
+    private GameLevel mediumLevel;
+    private GameLevel hardLevel;
+
     public static void main(String[] args) {
         new MainMenu().setVisible(true);
     }
-
 
     public MainMenu() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
         setSize(600,200);
         setLocationRelativeTo(null);
-        setComponents();
+        setGameLevels();
         setLayout(new GridLayout(1,3));
+        setComponents();
     }
 
-    private void setComponents() {
-
-        GameLevel easyLevel, mediumLevel, hardLevel;
+    private void setGameLevels() {
         easyLevel = new GameLevel("Easy", 9, 9, 10);
         mediumLevel = new GameLevel("Medium", 16, 16, 40);
         hardLevel = new GameLevel("Hard", 16, 30, 99);
+    }
 
-        JButton easy, medium, hard;
-        easy = new JButton("Easy");
-        easy.addActionListener(e -> {
-            new MinesweeperGame(easyLevel).setVisible(true);
-        });
-        add(easy);
-        medium = new JButton("Medium");
-        medium.addActionListener(e -> {
-            new MinesweeperGame(mediumLevel).setVisible(true);
-        });
-        add(medium);
-        hard = new JButton("Hard");
-        hard.addActionListener(e -> {
-            new MinesweeperGame(hardLevel).setVisible(true);
-        });
-        add(hard);
+    private void setComponents() {
+        add(new GameLevelButton(easyLevel));
+        add(new GameLevelButton(mediumLevel));
+        add(new GameLevelButton(hardLevel));
     }
 
 
