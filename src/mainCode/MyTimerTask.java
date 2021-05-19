6ca8fd98716d2b50edb700963e866994209c0617
Arios <ariosssmfm@gmail.com>
2021-05-19 21:08:02 +0300

    timerLabel

--- src/mainCode/MyTimerTask.java
+++ src/mainCode/MyTimerTask.java
@@ -7,14 +7,9 @@ import static mainCode.MinesweeperFrame.timeLabel;
 public class MyTimerTask extends TimerTask {
 
     private int secondsPassed = 0;
-    private HeaderLabel timeHeaderLabel;
-
-    public MyTimerTask() {
-        timeHeaderLabel = timeLabel;
-    }
 
     @Override
     public void run() {
-        timeHeaderLabel.reFresh(secondsPassed++);
+        timeLabel.reFresh(secondsPassed++);
     }
 }
