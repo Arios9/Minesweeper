e51c90b7694413806fac375ac72d7f7e8630d040
Arios <ariosssmfm@gmail.com>
2021-05-21 06:23:15 +0300

    extract the methods

--- src/mainCode/HighScore.java
+++ src/mainCode/HighScore.java
@@ -13,67 +13,117 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import static mainCode.MinesweeperFrame.timeLabel;
 import static mainCode.MinesweeperGame.gameLevel;
+import static mainCode.MyTimerTask.secondsPassed;
 
 
 public class HighScore {
 
     private static final String FILE_PATH = "src/files/records.xml";
     private static final String ROOT_TAG_NAME = "record";
-    private static String seconds, level;
+    private static String level;
+    private static int seconds;
     private static DocumentBuilderFactory documentBuilderFactory;
     private static DocumentBuilder documentBuilder;
     private static Document document;
     private static Element root;
+    private static NodeList nodeList;
+    private static File file;
 
     public static void checkForHighScore() {
-        seconds = timeLabel.getText();
-        level = gameLevel.getLevelText();
-
-        documentBuilderFactory = DocumentBuilderFactory.newInstance();
         try {
-            documentBuilder = documentBuilderFactory.newDocumentBuilder();
-
-            File file = new File(FILE_PATH);
-
-            if(file.isFile()){
-                document = documentBuilder.parse(file);
-                root = document.getDocumentElement();
-
-                NodeList nodeList = root.getElementsByTagName(level);
-                if(nodeList.getLength() == 0){
-                    Element element = document.createElement(level);
-                    element.setTextContent(seconds);
-                    root.appendChild(element);
-                }else{
-                    Node node = nodeList.item(0);
-                    String textContent = node.getTextContent();
-                    // check for better time
-                    if (Integer.parseInt(seconds) < Integer.parseInt(textContent)){
-                        node.setTextContent(String.valueOf(seconds));
-                    }
-                }
-            }else{
-                document = documentBuilder.newDocument();
-                root = document.createElement(ROOT_TAG_NAME);
-                document.appendChild(root);
-                Element element = document.createElement(level);
-                element.setTextContent(seconds);
-                root.appendChild(element);
-            }
-
-
-            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-            Transformer transformer = transformerFactory.newTransformer();
-            DOMSource domSource = new DOMSource(document);
-            FileOutputStream fileOutputStream = new FileOutputStream(file);
-            StreamResult streamResult = new StreamResult(fileOutputStream);
-            transformer.transform(domSource, streamResult);
+            tryingIsTheFirstStepToFailure();
         } catch (ParserConfigurationException | SAXException | IOException | TransformerException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void tryingIsTheFirstStepToFailure() throws ParserConfigurationException, IOException, SAXException, TransformerException {
+        createDocumentBuilder();
+        initializeVariables();
+        makeProperActions();
+        outPutDocumentToFile();
+    }
+
+    private static void createDocumentBuilder() throws ParserConfigurationException {
+        documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        file = new File(FILE_PATH);
+    }
+
+    private static void initializeVariables() {
+        seconds = secondsPassed;
+        level = gameLevel.getLevelText();
+    }
+
+    private static void makeProperActions() throws IOException, SAXException {
+        if(file.isFile())
+            makeProperActionsToExistingFile();
+        else
+            createDocumentAndNewElement();
+    }
+
+    private static void makeProperActionsToExistingFile() throws IOException, SAXException {
+        parseFileAndItsElements(file);
+        makeProperActionsToTheDocument();
+    }
+
+    private static void makeProperActionsToTheDocument() {
+        if(ElementExist())
+            setNewValueToElementIfTimeIsBetter();
+        else
+            createNewElementLevelRecord();
+    }
+
+    private static boolean ElementExist() {
+        return nodeList.getLength() > 0;
+    }
+
+    private static void setNewValueToElementIfTimeIsBetter() {
+        Node node = nodeList.item(0);
+        int currentHighScore = getTextContentFromNodeToInt(node);
+        if (seconds < currentHighScore)
+            node.setTextContent(String.valueOf(seconds));
+    }
+
+    private static int getTextContentFromNodeToInt(Node node){
+        String textContent = node.getTextContent();
+        return Integer.parseInt(textContent);
+    }
+
+    private static void createDocumentAndNewElement() {
+        createNewXMLDocument();
+        createNewElementLevelRecord();
+    }
+
+    private static void parseFileAndItsElements(File file) throws IOException, SAXException {
+        document = documentBuilder.parse(file);
+        root = document.getDocumentElement();
+        nodeList = root.getElementsByTagName(level);
+    }
+
+    private static void createNewElementLevelRecord() {
+        Element element = document.createElement(level);
+        String string = String.valueOf(seconds);
+        element.setTextContent(string);
+        root.appendChild(element);
+    }
+
+    private static void createNewXMLDocument() {
+        document = documentBuilder.newDocument();
+        root = document.createElement(ROOT_TAG_NAME);
+        document.appendChild(root);
+    }
+
+    private static void outPutDocumentToFile() throws FileNotFoundException, TransformerException {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource domSource = new DOMSource(document);
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        StreamResult streamResult = new StreamResult(fileOutputStream);
+        transformer.transform(domSource, streamResult);
     }
 }
--- src/mainCode/MyTimerTask.java
+++ src/mainCode/MyTimerTask.java
@@ -14,6 +14,6 @@ public class MyTimerTask extends TimerTask {
 
     @Override
     public void run() {
-        timeLabel.reFresh(secondsPassed++);
+        timeLabel.reFresh(++secondsPassed);
     }
 }
